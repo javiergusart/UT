c6351e76ad3724dd29c1ecac76e58341f85a3cef
javiergusart <javier.gusart@gmail.com>
2025-07-01 17:28:08 -0500

    started javascript

---
+++ JavaScript/Week-1/data-types-and-variables.js
@@ -0,0 +1,22 @@
+// number, boolean and string
+var x = 1, y, z = 2.718;
+console.log(x)
+console.log(y)
+
+console.log("Hello World!")
+
+console.log(typeof 1.5);
+console.log(typeof x);
+console.log(typeof z);
+
+//boolean
+var isItRaining = true, isItPouring = false;
+console.log(isItRaining)
+console.log(typeof (isItPouring))
+
+//string
+var name = "John", message = 'Good morning';
+console.log(name);
+console.log (typeof message);
+console.log (message.length);
+console.log(message + ' ' + name);
